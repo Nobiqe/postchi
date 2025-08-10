d7d319ff401c19bd1d68a2ff2c2933b6b0f76ae9
nobiqe <ebrahimnobiqe@gmail.com>
2025-08-10 17:36:58 +0330

    feat: the main file is completed, and now I'm going to develop the menu that contains bot options

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+from menu import MenuSystem
+
+
+async def main():
+    """ Main entry point of the application """
+    menu = MenuSystem()
+    await menu.run()
+
+
+if __name__ == "__name__":
+    
+    required_packages = [
+        "telethon",
+        "google_generativeai",
+        "asynico",
+        "sqlite3"
+    ]    
+
+    try:
+        import telethon
+        import google.generativeai
+        print("starting telegram multi-channel processor...\n")
+
+        asyncio.run(main())
+
+    except ImportError as e :
+        print(f"missing required package: {e}")
